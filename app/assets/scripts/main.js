d57df1fc0d62e5096ab5c5abe69bff0ddcc734af
Seth Fitzsimmons <seth@mojodna.net>
2017-09-29 14:43:51 -0700

    Include completeness as text

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -73,6 +73,7 @@ function makeProject (project, projectOrder) {
   $(`ul li:nth-child(${projectOrder}) .title`).html(props.name);
   $(`ul li:nth-child(${projectOrder}) .HOT-Progress`).html(`<p>${projDone}%</p>`);
   $(`ul li:nth-child(${projectOrder}) .HOT-Progress`).attr('title', `${projDone}% complete`);
+  $(`ul li:nth-child(${projectOrder}) .HOT-Details .completeness`).html(`<strong>${projDone}%</strong> complete`);
   $(`ul li:nth-child(${projectOrder}) .HOT-Map`).attr('id', 'Map-' + project.id);
 
   // Drop a map into the HOT-Map div
