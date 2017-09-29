9dcce37fcee19b230b3f9e23c4ac86a2f99f4486
Seth Fitzsimmons <seth@mojodna.net>
2017-09-28 18:11:39 -0700

    Don't output repo URL

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -6,7 +6,7 @@ if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; 
   echo "Get ready, we're pushing to gh-pages!"
   git config user.name "Travis-CI"
   git config user.email "travis@somewhere.com"
-  node_modules/.bin/gh-pages -d _site -b gh-pages -r "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
+  node_modules/.bin/gh-pages -x -d _site -b gh-pages -r "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
 else
   echo "Not a publishable branch so we're all done here"
 fi
