88f183cfab1e9eec91a8b5daa8aff0b9c5c59dbe
AfiMaameDufie <afimaamedufie@gmail.com>
2023-03-09 19:07:53 +0000

    Changed gh-pages-branch test back to original

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -6,7 +6,7 @@ if [ $CIRCLE_BRANCH = ${DEPLOY_BRANCH} ]; then
   echo "Get ready, we're pushing to gh-pages!"
   git config user.name "CIRCLECI"
   git config user.email "circleci@somewhere.com"
-  node_modules/.bin/gh-pages -x -d _site -b gh-pages-test -r $CIRCLE_REPOSITORY_URL
+  node_modules/.bin/gh-pages -x -d _site -b gh-pages -r $CIRCLE_REPOSITORY_URL
 else
   echo "Not a publishable branch so we're all done here"
 fi
