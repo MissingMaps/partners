4747dadde14017abf8a332bb41bd672834a36740
AfiMaameDufie <afimaamedufie@gmail.com>
2023-03-09 16:40:55 +0000

    Specified Circle CI branch and repo url

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -2,11 +2,11 @@
 set -e # halt script on error
 
 # If this is the deploy branch, push it up to gh-pages
-if [ $CIRCLE_PULL_REQUEST = "false" ] && [ $CIRCLE_BRANCH = ${DEPLOY_BRANCH} ]; then
+if [ $CIRCLE_BRANCH = ${DEPLOY_BRANCH} ]; then
   echo "Get ready, we're pushing to gh-pages!"
   git config user.name "CIRCLECI"
   git config user.email "circleci@somewhere.com"
-  node_modules/.bin/gh-pages -x -d _site -b gh-pages -r "https://${GH_TOKEN}@github.com/${CIRCLE_REPOSITORY_URL}.git"
+  node_modules/.bin/gh-pages -x -d _site -b gh-pages -r $CIRCLE_REPOSITORY_URL
 else
   echo "Not a publishable branch so we're all done here"
 fi
