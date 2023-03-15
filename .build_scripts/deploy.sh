0c3deee21102df2591af3e53b784e3b53a7423a8
AfiMaameDufie <afimaamedufie@gmail.com>
2023-03-15 12:26:43 +0000

    Fixing the deploy the branch

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -6,7 +6,7 @@ if [ $CIRCLE_BRANCH = ${DEPLOY_BRANCH} ]; then
   echo "Get ready, we're pushing to gh-pages!"
   git config user.name "CIRCLECI"
   git config user.email "circleci@somewhere.com"
-  node_modules/.bin/gh-pages -x -d _site -b gh-pages -r $CIRCLE_REPOSITORY_URL
+  node_modules/.bin/gh-pages -x -d _site -b gh-pages -r "https://${GH_TOKEN}@github.com/${CIRCLE_REPOSITORY_URL}.git"
 else
   echo "Not a publishable branch so we're all done here"
 fi
