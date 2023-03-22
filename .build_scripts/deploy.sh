b0d1162c0a7a3182ac5d4d061ee415e46d97b1ec
AfiMaameDufie <afimaamedufie@gmail.com>
2023-03-22 16:36:51 +0000

    Publishing branch

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -6,7 +6,7 @@ if [ $CIRCLE_BRANCH = ${DEPLOY_BRANCH} ]; then
   echo "Get ready, we're pushing to gh-pages!"
   git config user.name  $USERNAME
   git config user.email $EMAIL
-  node_modules/.bin/gh-pages -x -d _site -b gh-pages -r "https://github.com/${CIRCLE_REPOSITORY_URL}.git"
+  node_modules/.bin/gh-pages -d _site -b gh-pages -r "https://github.com/${CIRCLE_REPOSITORY_URL}.git"
 else
   echo "Not a publishable branch so we're all done here"
 fi
