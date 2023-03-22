37f8535f75dee5b83b60e462301b953d0c3a7f29
AfiMaameDufie <afimaamedufie@gmail.com>
2023-03-22 14:47:02 +0000

    Configuring git to script

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -4,9 +4,9 @@ set -e # halt script on error
 # If this is the deploy branch, push it up to gh-pages
 if [ $CIRCLE_BRANCH = ${DEPLOY_BRANCH} ]; then
   echo "Get ready, we're pushing to gh-pages!"
-  git config user.name "CIRCLECI"
-  git config user.email "circleci@somewhere.com"
-  node_modules/.bin/gh-pages -x -d _site -b gh-pages -r "https://${GH_TOKEN}@github.com/${CIRCLE_REPOSITORY_URL}.git"
+  git config user.name  $USERNAME
+  git config user.email $EMAIL
+  node_modules/.bin/gh-pages -x -d _site -b gh-pages-test -r "https://github.com/${CIRCLE_REPOSITORY_URL}.git"
 else
   echo "Not a publishable branch so we're all done here"
 fi
