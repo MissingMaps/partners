4696ee43308e112f64098231bfc4004cb1aebc17
AfiMaameDufie <afimaamedufie@gmail.com>
2023-03-22 17:08:27 +0000

    Publishing branch

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -3,8 +3,8 @@ set -e # halt script on error
 
 # If this is the deploy branch, push it up to gh-pages
 if [ $CIRCLE_BRANCH = ${DEPLOY_BRANCH} ]; then
-  echo "CIRCLE_BRANCH: $CIRCLE_BRANCH"
-  echo "\$DEPLOY_BRANCH:  ${DEPLOY_BRANCH}"
+  echo "CIRCLE_BRANCH: " $CIRCLE_BRANCH
+  echo "\$DEPLOY_BRANCH: " ${DEPLOY_BRANCH}
   echo "Get ready, we're pushing to gh-pages!"
   git config user.name  $USERNAME
   git config user.email $EMAIL
